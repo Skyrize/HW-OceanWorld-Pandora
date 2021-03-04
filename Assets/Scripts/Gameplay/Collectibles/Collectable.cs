53d8259a0e4fd9d71c5efc75e47f6742dd3a1db6
Maxime Houis <maxime.houis@epitech.eu>
2021-03-04 17:11:22 +0100

    feat: collectible resources

---
+++ Assets/Scripts/Gameplay/Collectibles/Collectable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string Name;
+    public uint Count = 1;
+    [Min(0f)] public float Weight = 1.0f;
+}
---
+++ Assets/Scripts/Gameplay/Collectibles/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private readonly Dictionary<string, uint> m_content = new Dictionary<string, uint>();
+
+    [HideInInspector] public float TotalWeight;
+
+    public void Collect(GameObject obj)
+    {
+        var collectable = obj.GetComponent<Collectable>();
+
+        if (!collectable)
+            throw new NullReferenceException("Collider is not collectable");
+
+        if (!m_content.ContainsKey(collectable.Name))
+            m_content.Add(collectable.Name, 0);
+
+        m_content[collectable.Name] += collectable.Count;
+        TotalWeight += collectable.Weight;
+
+        Destroy(obj);
+        DebugPrintInventory();
+    }
+
+    public void Remove(string objectName, uint count = 1)
+    {
+        if (!m_content.ContainsKey(objectName))
+            throw new KeyNotFoundException("Object is not in inventory");
+
+        m_content[objectName] -= count;
+
+        if (m_content[objectName] == 0)
+            m_content.Remove(objectName);
+    }
+
+    private void DebugPrintInventory()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Inventory:");
+        foreach (var item in m_content)
+        {
+            Debug.Log(item.Key + " " + item.Value);
+        }
+#endif
+    }
+}
