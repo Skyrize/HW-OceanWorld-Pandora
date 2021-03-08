5d5e60f64257cc608491b2465e23947d2809f4d8
Clément <clement.baudonnel@epitech.eu>
2021-03-08 03:42:06 +0100

    feat : placement | fix : materials
    - fixed assets materials,
    - added placing with UI but Collision still WIP,
    - added TMP prefabs and code to refactor later with inventory,
    - added functionalities to InputManager

--- Assets/Scripts/Gameplay/CrewManager.cs
+++ Assets/Scripts/Gameplay/CrewManager.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[System.Serializable]
+public class TMP_InventoryItem {
+    public GameObject item;
+    public Sprite icon;
+}
+
 public class CrewManager : MonoBehaviour
 {
+    [Header("References")]
+
+    [SerializeField] protected PlacableInventoryUI placableInventoryUI = null;
+    [SerializeField] protected ItemPlacer itemPlacer = null;
     [Header("Runtime")]
     [SerializeField] protected bool toggle = false;
     // Start is called before the first frame update
@@ -12,15 +22,33 @@ public class CrewManager : MonoBehaviour
     void Start()
     {
         events = GetComponent<EventBinder>();
+        placableInventoryUI.onSelected.AddListener(SelectPlacableItem);
+        Exit();
+    }
+
+    public void SelectPlacableItem(TMP_InventoryItem item)
+    {
+        itemPlacer.CurrentItem = item;
+    }
+
+    public void Enter()
+    {
+        events.CallEvent("Enter");
+    }
+
+    public void Exit()
+    {
+        events.CallEvent("Exit");
+        itemPlacer.CurrentItem = null;
     }
 
     public void Toggle()
     {
         toggle = !toggle;
         if (toggle) {
-            events.CallEvent("Enter");
+            Enter();
         } else {
-            events.CallEvent("Exit");
+            Exit();
         }
     }
 
---
+++ Assets/Scripts/UI/PlacableInventoryUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlacableInventoryUI : MonoBehaviour
+{
+    [Header("Events")]
+    [SerializeField] public ItemEvent onSelected = new ItemEvent();
+    [Header("References")]
+    [SerializeField] protected RectTransform placableObjectBarContent = null;
+    [SerializeField] protected GameObject placableObjectButtonPrefab = null;
+    [Header("TMP")]
+    [SerializeField]
+    TMP_InventoryItem[] items = new TMP_InventoryItem[3];
+    
+    public void SelectPlacableItem(TMP_InventoryItem item)
+    {
+        onSelected.Invoke(item);
+    }
+
+    private void OnEnable() {
+        // TODO: link with real inventory
+        foreach (var item in items)
+        {
+            GameObject button = GameObject.Instantiate(placableObjectButtonPrefab, placableObjectBarContent); // TODO: easy but dirty. Maybe add them along when adding to inventory
+            SelectableObjectButtonUI buttonUI = button.GetComponent<SelectableObjectButtonUI>();
+
+            buttonUI.item = item;
+            buttonUI.onSelected.AddListener(this.SelectPlacableItem);
+        }
+    }
+
+    private void OnDisable() {
+        placableObjectBarContent.ClearChilds(); // TODO: easy but dirty. Maybe remove them along when adding to inventory
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
