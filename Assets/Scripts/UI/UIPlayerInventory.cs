b386d42073c7bbafcb091e65586056fd2ffe0c92
Mélanie M <marquesmelanie5@gmail.com>
2021-03-08 14:01:10 +0100

    [FIX] minigun and cannon merchant

---
+++ Assets/Scripts/UI/UIPlayerInventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerInventory : MonoBehaviour
+{
+    public Inventory inventoryPlayer;
+    public GameObject inventoryItem;
+
+    void Start()
+    {
+        gameObject.transform.Find("Money/MoneyDisplay").GetComponent<Text>().text = inventoryPlayer.Money.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Find("Money/MoneyDisplay").GetComponent<Text>().text = inventoryPlayer.Money.ToString();
+    }
+}
