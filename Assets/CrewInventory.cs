500d268562dc8e544c9cc69b87f9ce787ad38ab5
Clément <clement.baudonnel@epitech.eu>
2021-03-15 22:28:21 +0100

    weird stash merge

---
+++ Assets/CrewInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/CrewInventory")]
+public class CrewInventory : Inventory
+{
+    [SerializeField] protected uint maxCrewSize = 2;
+    public uint MaxCrewSize => maxCrewSize;
+    public List<CrewMember> crewMembers = new List<CrewMember>();
+
+    override protected ClonableSO Clone()
+    {
+        CrewInventory clone = base.Clone() as CrewInventory;
+
+        clone.crewMembers.Clear();
+        foreach (CrewMember crewMember in crewMembers)
+        {
+            clone.crewMembers.Add(ClonableSO.Clone<CrewMember>(crewMember));
+        }
+        return clone;
+    }
+
+    public void AddCrewMember(CrewMember newCrewMember)
+    {
+        if (crewMembers.Count == maxCrewSize) {
+            throw new System.Exception($"You have reached your max crew capacity '{maxCrewSize}'.");
+        }
+        if (crewMembers.Find(crewMember => crewMember.Name == newCrewMember.Name)) {
+            throw new System.Exception($"Can't add two CrewMembers with the same name '{newCrewMember.Name}' for now");
+        }
+        crewMembers.Add(ClonableSO.Clone<CrewMember>(newCrewMember));
+    }
+}
