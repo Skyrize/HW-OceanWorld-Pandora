5947207a841c8254da8ce79bc139eb203d65f9ee
Clément <clement.baudonnel@epitech.eu>
2021-03-16 16:13:43 +0100

    feat : water manager

---
+++ Assets/Editor/WaterManagerEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaterManager))]
+class WaterManagerEditor : Editor {
+
+    public void Generate(Transform transform, GameObject waterPrefab, uint xSize, uint zSize)
+    {
+        transform.ClearChilds();
+        Vector3 size = waterPrefab.GetComponent<Renderer>().bounds.size;
+        Debug.Log("size" + size.ToString());
+        float xStartPos = -(float)xSize / 2.0f * size.x + size.x / 2.0f;
+        float zStartPos = -(float)zSize / 2.0f * size.z + size.z / 2.0f;
+        for (int x = 0; x != xSize; x++) {
+            Debug.Log("x" + x);
+            for (int z = 0; z != zSize; z++) {
+            Debug.Log("z" + z);
+                Vector3 pos = new Vector3(xStartPos + x * size.x, 0, zStartPos + z * size.z);
+                var water = PrefabUtility.InstantiatePrefab(waterPrefab, transform) as GameObject;
+                water.transform.position = pos;
+                //  Instantiate(waterPrefab, pos, Quaternion.identity, transform);
+            }
+        }
+        EditorUtility.SetDirty(this);
+    }
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        if(GUILayout.Button("Generate")) {
+            WaterManager myScript = (WaterManager)target;
+
+            Generate(myScript.transform, myScript.waterPrefab, myScript.xSize, myScript.zSize);
+        }
+    }
+}
