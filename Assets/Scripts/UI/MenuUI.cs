1c61be113f3584b815966458b9e94dffc662353b
Mélanie M <marquesmelanie5@gmail.com>
2021-03-21 00:47:33 +0100

    [ADD] main menu

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
@@ -21,10 +22,23 @@ public class MenuUI : MonoBehaviour
     public void EnterInMenu()
     {
         pauseManager.Pause();
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
     }
 
     public void ExitMenu()
     {
-        pauseManager.Unpause();
+        if (pauseManager.paused)
+        {
+            pauseManager.Unpause();
+            UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("MainMenu");
+        }
+        else
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
