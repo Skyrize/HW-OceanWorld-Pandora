9a1a725c54b3a4faa082e8cb240d4c50dfb4eeb3
Mélanie M <marquesmelanie5@gmail.com>
2021-03-21 21:15:11 +0100

    [ADD] gameover menu

---
+++ Assets/Scripts/Gameplay/Menu/ScenesTransition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesTransition : MonoBehaviour
+{
+    public HealthComponent healthPlayer;
+    private bool isLoaded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(healthPlayer.Health <= 90 && !isLoaded)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOverScene", LoadSceneMode.Additive);
+            isLoaded = true;
+        }            
+    }
+}
---
+++ Assets/Scripts/UI/MenuUI/GameOver/GameOverUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    
+    public List<string> listOfLevelScenes = null;
+
+    // Start is called before the first frame update
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        foreach(string scene in listOfLevelScenes)
+        {
+            if (UnityEngine.SceneManagement.SceneManager.GetSceneByName(scene).isLoaded)
+            {
+                UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("GameOverScene");
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
+            }
+
+        }
+    }
+}
